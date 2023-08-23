fa5e7e83c830b71d73a178ffa67662a0ce22375b
azx79115 <azx79115@gmail.com>
2023-08-23 11:59:34 +0800

    更新server URL

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:5000" || process.env.REACT_APP_YOUR_HOST;
+export const host = process.env.REACT_APP_YOUR_HOST;
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
